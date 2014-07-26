02547b42cab580039b8f7ec19614cc9d903312b6
Albrecht Gradmann <agradmann@gmail.com>
2014-07-26 13:16:14 +0200

    improved code after SO Post

--- R/dsdparser.R
+++ R/dsdparser.R
@@ -6,22 +6,17 @@ file <- "http://ec.europa.eu/eurostat/SDMX/diss-web/rest/datastructure/ESTAT/DSD
 content <- getURL(file, httpheader = list('User-Agent' = 'R-Agent'))
 root <- xmlRoot(xmlInternalTreeParse(content, useInternalNodes = TRUE))
 
+# get Namespaces
+ns <- xmlNamespaceDefinitions(root, simplify = TRUE)
+
 # get Nodeset of Codelists and its length
 nodes <- getNodeSet(root,"//str:Codelist")
-nn <- length(nodes)
 
-# Create nested List of all Codes and Names
-codelistAll <- lapply(seq(nn),function(i){
-  xpathSApply(root,paste0("//str:Codelist[",i,"]/str:Code"),xmlGetAttr, "id")
-})
 
-namelistAll <- lapply(seq(nn),function(i){
-  xpathSApply(root,paste0("//str:Codelist[",i,"]/str:Code/com:Name"),xmlValue)
-})
+alldfList <- lapply(nodes, function(x){ data.frame(
+  codes= xpathSApply(x, ".//str:Code" , xmlGetAttr, "id", namespaces=ns),
+  names= xpathSApply(x, ".//str:Code" , xmlValue, namespaces=ns) )})
 
-# Create a list of dataframes from the nested lists
-alldfList <-lapply(seq(nn),function(i) data.frame(codes=codelistAll[[i]],names=namelistAll[[i]]))
-
-# Name the list items like the nodes
 names(alldfList)  <- sapply(nodes, xmlGetAttr,"id")
 
+alldfList
