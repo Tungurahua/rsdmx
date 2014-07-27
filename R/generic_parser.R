e7ddeddb09a66c8bfa0b963c08d0e87b60cd8b3b
Albrecht Gradmann <agradmann@gmail.com>
2014-07-27 13:42:47 +0200

    Stash

--- R/generic_parser.R
+++ R/generic_parser.R
@@ -4,7 +4,7 @@ library(plyr)
 
 # REST resource for DSD of nama_gdp_c
 # downloading, parsing XML an setting root
-file <- "http://ec.europa.eu/eurostat/SDMX/diss-web/rest/data/cdh_e_fos/..PC.FOS1./?startperiod=2005&endPeriod=2013"
+file <- "http://ec.europa.eu/eurostat/SDMX/diss-web/rest/data/cdh_e_fos/..PC.FOS1.BE/?startperiod=2005&endPeriod=2013"
 content <- getURL(file, httpheader = list('User-Agent' = 'R-Agent'))
 root <- xmlRoot(xmlInternalTreeParse(content, useInternalNodes = TRUE))
 
@@ -13,7 +13,10 @@ n_Series <- getNodeSet(root,"//generic:Series")
 nn_Series <- length(n_Series)
 
 # get the list of value attributes
-Codes <- xpathSApply(root,"//generic:SeriesKey[1]/generic:Value",xmlGetAttr,"id")
+Codes <- unique(xpathSApply(root,"//generic:SeriesKey/generic:Value",xmlGetAttr,"id"))
+Attributes  <- unique(xpathSApply(root,"//generic:Attributes/generic:Value",xmlGetAttr,"id"))
+
+
 
 # namespace
 ns <- xmlNamespaceDefinitions(root, simplify = TRUE)
@@ -31,13 +34,24 @@ df1 <- as.data.frame(sapply(Codes, function(i){xpathSApply(root, paste0(".//gene
 df1 <- df1[rep(seq_len(nrow(df1)), time=obs_per_ser),]
 
 
-
 ## time and value columns
 df2 <- data.frame(
   time  = xpathSApply(root, ".//generic:ObsDimension" , xmlGetAttr, "value", namespaces=ns),
   value = xpathSApply(root, ".//generic:ObsValue" , xmlGetAttr, "value", namespaces=ns))
   
 
+## Attributes
+df3 <- data.frame(
+  Att1 <- xpathSApply(root, "//generic:Attributes/generic:Value[@id='OBS_STATUS']",
+                      function(x){xmlGetAttr(x, "value", default="Hicks")}),
+  
+  Att2 <- xpathSApply(root, "//generic:Attributes/generic:Value[@id='OBS_FLAG']", 
+                      function(x){xmlGetAttr(x, "value")})
+  
+  length(getNodeSet(root, "//generic:Attributes/generic:Value[@id='OBS_STATUS']"))
+   
+  
+  Att2
 cbind(df1,df2)
 
 
