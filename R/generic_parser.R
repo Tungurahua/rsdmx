4b9bbdb2ed7a302f93a77adae0f87a70e8ffb53c
Albrecht Gradmann <agradmann@gmail.com>
2014-07-28 02:16:46 +0200

    code decoration

--- R/generic_parser.R
+++ R/generic_parser.R
@@ -1,27 +1,8 @@
 read_eurostat <- function(flowRef, key, start, end){
-#require(XML)
-#require(RCurl)
 
-#xmlGetNodeAttr <- function(n, xp, attr,ns,default=NA) {
-#  nds<-getNodeSet(n, xp, namespaces=ns)
-#  if(length(nds)<1) {
-#    return(default)
-#  } else {
-#    sapply(nds, xmlGetAttr, attr, default)
-#  }
-#}
-
-
-
-# for debugging
-#flowRef = "cdh_e_fos" 
-#key = "..PC.FOS1.BE"
-#start = 2005
-#end =   2011
-
+# Prepare REST Call from function arguments
 file <- paste0("http://ec.europa.eu/eurostat/SDMX/diss-web/rest/data/",flowRef,"/",key,"/?startperiod=",start,"&endPeriod=",end)
   
-# REST resource for DSD of nama_gdp_c
 # downloading, parsing XML an setting root
 content <- getURL(file, httpheader = list('User-Agent' = 'R-Agent'))
 root <- xmlRoot(xmlInternalTreeParse(content, useInternalNodes = TRUE))
@@ -31,9 +12,9 @@ n_Series <- getNodeSet(root,"//generic:Series")
 nn_Series <- length(n_Series)
 
 # get the list of value attributes
+# [TODO] Attributes are still hard-coded below
 Codes <- unique(xpathSApply(root,"//generic:SeriesKey/generic:Value",xmlGetAttr,"id"))
 Attributes  <- unique(xpathSApply(root,"//generic:Attributes/generic:Value",xmlGetAttr,"id"))
-
 
 
 # namespace
@@ -47,18 +28,12 @@ obs_per_ser <- sapply(seq(nn_Series),
 
 
 
-## Series Keys
+## Series Keys. These need to be replicated since multiple observations (years) can be 
 df1 <- as.data.frame(sapply(Codes, function(i){xpathSApply(root, paste0(".//generic:Value[@id='",i,"']") , xmlGetAttr, "value", namespaces=ns)}))
 df1 <- df1[rep(seq_len(nrow(df1)), time=obs_per_ser),]
 
 
-
-
-
-
-
-## Attributes
-## Still need to figure out how to parse  Attributes
+## Values and Attributes
 df2 <- do.call(rbind, lapply(getNodeSet(root,"//generic:Obs"), function(x) {
   data.frame(
     dimension=xmlGetNodeAttr(x, "./generic:ObsDimension","value",NA,ns),
@@ -69,7 +44,7 @@ df2 <- do.call(rbind, lapply(getNodeSet(root,"//generic:Obs"), function(x) {
 }))
 
 
-
+# Combine the dfs
 df <- cbind(df1,df2)
 return(df)
 }
